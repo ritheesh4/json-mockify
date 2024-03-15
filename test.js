d40f094df37b4a22b474a8ae3aa90c2d71a23c34
ritheesh4 <ritheesh.4@gmail.com>
2024-03-15 11:33:14 +0530

    Configure build scripts

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const createServer = require('./index');
+const createServer = require('./src/index');
 
 // Provide the path to the custom configuration file
-const customConfigPath = '../defaultConfig.json';
+const customConfigPath = '../../defaultConfig.json';
 
 // Create server with custom configuration
 createServer(customConfigPath);
